1f8b0cd9b180aa101bb7c01d059b915d192cba09
Kanishka Mallawaarachchi <mallawarachchikanishka@gmail.com>
2023-04-29 00:04:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Signin from './pages/sign-in/SignIn';
+import Dashboard from './pages/dashboard/Dashboard.js'
+import Checkout from './pages/checkout/Checkout'
+
+function App() {
+  return (
+    <Dashboard/>
+  );
+}
+
+export default App;
